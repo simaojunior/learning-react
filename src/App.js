25a4c05e47bb36b5b208194fffb1a36efda7f024
Simão Junior <simao.msjr@gmail.com>
2020-12-03 16:11:35 -0300

    Jsx concepts (#3)
    * chore: Remove unnecessary files.
    
    * chore: Add some reset css.
    
    * chore: Learning some of concepts of JSX.
    
    * chore: Add the file .eslintcache on gitignore.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const luana = {
+    client: 'Luana',
+    age: 27,
+    shopping: [
+      {
+        name: 'Notebook',
+        price: 'R$ 2500',
+      },
+      {
+        name: 'Refrigerator',
+        price: 'R$ 3000',
+      },
+      {
+        name: 'Smartfhone',
+        price: 'R$ 1500',
+      },
+    ],
+    active: true,
+  };
+
+  const kelly = {
+    client: 'Kelly',
+    age: 29,
+    shopping: [
+      {
+        name: 'Notebook',
+        price: 'R$ 2500',
+      },
+      {
+        name: 'Refrigerator',
+        price: 'R$ 3000',
+      },
+      {
+        name: 'Smartfhone',
+        price: 'R$ 1500',
+      },
+      {
+        name: 'PS5',
+        price: 'R$ 5500',
+      },
+    ],
+    active: false,
+  };
+
+  const data = kelly;
+
+  const total = data.shopping
+    .map((item) => Number(item.price.replace('R$ ', '')))
+    .reduce((a, b) => a + b);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>Nome: {data.client}</p>
+      <p>Idade: {data.age}</p>
+      <p>Total: R$ {total} </p>
+      <p>
+        Situação:{' '}
+        <span style={{ color: data.active ? 'green' : 'red' }}>
+          {data.active ? 'Ativo' : 'Inativo'}
+        </span>
+      </p>
+      {total > 1000 && <p>Você está gastando demais.</p>}
     </div>
   );
 }
